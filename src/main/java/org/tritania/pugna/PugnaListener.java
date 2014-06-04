f665e9e87f6c1736c63f7960d720510520ce06c6
Tritania <erikwilson@magnorum.com>
2014-06-04 02:05:10 -0300

    Skeletal bounty system.

--- src/main/java/org/tritania/pugna/PugnaListener.java
+++ src/main/java/org/tritania/pugna/PugnaListener.java
@@ -21,6 +21,7 @@ import org.bukkit.entity.Player;
 import org.bukkit.event.player.PlayerEvent;
 import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
+import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.EventPriority;
 import org.bukkit.event.Listener;
@@ -53,6 +54,12 @@ public class PugnaListener implements Listener
     
     @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
     public void onPlayerLeave(PlayerQuitEvent event)
+    {
+       
+    }
+    
+    @EventHandler(priority = EventPriority.LOW, ignoreCancelled = true)
+    public void onPlayerDeath(PlayerDeathEvent event)
     {
        
     }
